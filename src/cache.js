c113607428107aab3c2d27db06c3db03c8479ca8
MaksTue <pro100max48@gmail.com>
2023-09-26 14:57:19 +0300

    add cahe

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,35 @@
-class Cache{
+class Cache {
+  constructor() {
+    this.data = new Map();
+  }
 
+  set(key, value, accessCount = 1) {
+    this.data.set(key, { value, accessCount });
+  }
+
+  get(key) {
+    const entry = this.data.get(key);
+
+    if (!entry.value) {
+      return null;
+    }
+
+    if (entry.accessCount === 0) {
+      this.data.delete(key);
+      return null;
+    }
+
+    entry.accessCount -= 1;
+    return entry.value;
+  }
+
+  getCacheStatistics() {
+    const statistics = Array.from(this.data.entries()).map(([key, value]) => ({
+      key,
+      value: value.value,
+      accessCount: value.accessCount,
+    }));
+    return statistics;
+  }
 }
-export {Cache}
+export { Cache };
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,49 @@
-import {Cache} from "../src/cache";
+import { Cache } from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+test("should set and get values", () => {
+  const cache = new Cache();
+  cache.set("key1", "value1");
+  cache.set("key2", "value2", 2);
+
+  expect(cache.get("key1")).toBe("value1");
+  expect(cache.get("key2")).toBe("value2");
+  expect(cache.get("key2")).toBe("value2");
+});
+
+test("should decrement accessCount", () => {
+  const cache = new Cache();
+  cache.set("key1", "value1", 3);
+
+  expect(cache.get("key1")).toBe("value1");
+  expect(cache.get("key1")).toBe("value1");
+  expect(cache.get("key1")).toBe("value1");
+  expect(cache.get("key1")).toBe(null);
+});
+
+test("should remove entries with accessCount 0", () => {
+  const cache = new Cache();
+  cache.set("key1", "value1", 2);
+
+  expect(cache.get("key1")).toBe("value1");
+  expect(cache.get("key1")).toBe("value1");
+  expect(cache.get("key1")).toBe(null);
+  expect(cache.getCacheStatistics()).toEqual([]);
+});
+
+test("should return cache statistics", () => {
+  const cache = new Cache();
+  cache.set("key1", "value1", 2);
+  cache.set("key2", "value2", 1);
+
+  expect(cache.getCacheStatistics()).toEqual([
+    { key: "key1", value: "value1", accessCount: 2 },
+    { key: "key2", value: "value2", accessCount: 1 },
+  ]);
+});
+
+test("should return null when value is null", () => {
+  const cache = new Cache();
+  cache.set("key1", null, 2);
+
+  expect(cache.get("key1")).toBe(null);
 });
